2ce335d8703a8dda79443a250d79e43536b69d82
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-09-15 20:37:02 -0400

    move files around

--- BlackLion.Web.Api/Startup.cs
+++ BlackLion.Web.Api/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HotChocolate;
+using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BlackLion.Web.Api
 {
@@ -25,7 +20,14 @@ namespace BlackLion.Web.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddGraphQL(sp => SchemaBuilder
+                    .New()
+                    .AddQueryType<Query>()
+                    .AddServices(sp)
+                    .Create())
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,7 @@ namespace BlackLion.Web.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseGraphQL();
             app.UseMvc();
         }
     }
